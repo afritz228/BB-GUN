7117d8c7eff61b4914a6ea76ecf4267b67f846e5
afritz228 <afritz228@gmail.com>
2018-07-25 12:15:54 -0500

    website files

---
+++ bee.js
@@ -0,0 +1,19 @@
+function hideshow_home(){
+display: block
+display: none
+}
+
+function hideshow_plants(){
+display: block
+display: none
+}
+
+function hideshow_insects(){
+display: block
+display: none
+}
+
+function hideshow_gallery(){
+display: block
+display: nones
+}
